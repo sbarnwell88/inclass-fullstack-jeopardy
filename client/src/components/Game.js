1f302da2c67a6d5b9a9b9a75f7474c1c3f8e3e70
sbarnwell88 <susy6688@gmail.com>
2017-08-09 14:51:13 -0400

    created user profile

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Game extends Component {
+    constructor() {
+        super();
+        this.state = {
+            id: '',
+            user: '',
+            points: 0,
+            categories: [],
+            board: []
+        }
+    }
+    
+    componentWillMount(){
+        const id = this.props.match.params.gameId;
+        axios.get(`/api/game/${id}`).then((res) => {
+            this.setState({
+                id: res.data._id,
+                user: res.data.user,
+                points: res.data.points,
+                categories: res.data.categories,
+                board: res.data.board
+            })
+        })
+    }
     render() {
         return (
             <div>
-                Hello from Game
-                {this.props.match.params.gameId}
+                <h1>Jeopardy</h1>
+                <h3>Hello {this.state.user}</h3>
+                <h3>Points: {this.state.points}</h3>
             </div>
         );
     }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import axios from 'axios';
 
 class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+            games: []
+        }
+    }
+    componentWillMount() {
+        axios.get('/api/game').then((res) => {
+            this.setState({games: res.data});
+        })
+    }
     render() {
         return (
             <div>
-                Hello from Home
+                <h1>JEOPARDY</h1>
+                <form>
+                    <input type="text"/>
+                    <button>New Game</button>
+                </form>
+
+                <ul>
+                    {this.state.games.map((game, i) => {
+                        return (
+                        <li key={i}>
+                            <Link to={`/game/${game._id}`}> {game.user}'s Game</Link>
+                        </li>
+                        );
+                    })}
+                </ul>
             </div>
         );
     }
