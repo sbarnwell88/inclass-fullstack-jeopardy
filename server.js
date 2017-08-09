5b2e7f478f72dfe61b467ab0ad84d3bb82f887d8
sbarnwell88 <susy6688@gmail.com>
2017-08-09 11:07:55 -0400

    added npm and yarn to work concurrently

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log('App listening on: ' + PORT);
 });
